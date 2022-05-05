c970d89d5f6dd0ee1981cf7fe1a1a3885f875121
Rajat Raj <rajatyadav2k17@gmail.com>
2022-05-05 08:57:26 +0530

    Time: 197 ms (5.30%), Space: 26.6 MB (56.68%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-     int findUnsortedSubarray(vector<int>& nums) {
-         int mini=nums[0],maxi=nums[nums.size()-1];
-         
-         int l=-1,r=0;
-         for(int i=1;i<nums.size();i++)
-         {
-             if(mini>nums[i])
-             {
-                 l=i; //left most unsettled
-             }
-             else
-                 mini=nums[i];
-         }
-         for(int i=nums.size()-2;i>=0;i--)
-         {
-             if(maxi<nums[i])
-             {
-                 r=i;
-             }
-             else
-                maxi=nums[i];
-         }
-         return l-r+1;
+    int findUnsortedSubarray(vector<int>& nums) {
+        int s=-1,e=0;
+        int mini=nums[0], maxi=nums[nums.size()-1];
+        for(int i=1;i<nums.size();i++)
+        {
+            if(mini>nums[i])
+            {
+                s=i;
+            }
+            else
+            mini=nums[i];
+        }
+        for(int i=nums.size()-2;i>=0;i--)
+        {
+            if(maxi<nums[i])
+            {
+                cout<<maxi<<" "<<i<<endl;
+                e=i;
+            }
+            else
+            maxi=nums[i];
+        }
+        return s-e+1;
     }
 };
