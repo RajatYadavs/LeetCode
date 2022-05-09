c92d54e0f43ad775b4941054e79a3e2db5cdacd2
Rajat Raj <rajatyadav2k17@gmail.com>
2022-05-09 09:47:58 +0530

    Time: 19 ms (69.49%), Space: 21.8 MB (7.76%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -3,22 +3,23 @@ int ways(int idx,int amount,vector<int>& coins,vector<vector<int>>&dp)
     if(amount==0)
         return dp[0][idx]=1;
     
+    else if(idx==coins.size())
+        return 0;
+    
     else if(dp[amount][idx]!=-1)
         return dp[amount][idx];
     
     int t=0;
-    for(int i=idx;i<coins.size();i++)
-    {
-        if(coins[i]<=amount)
-        {
-            t+=ways(i,amount-coins[i],coins,dp);
-        }
-    }
+    
+    if(coins[idx]<=amount)
+        t=ways(idx,amount-coins[idx],coins,dp)+ways(idx+1,amount,coins,dp);
+    
     return dp[amount][idx]=t;
 }
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        sort(coins.begin(),coins.end());
         vector<vector<int>>dp(amount+1,vector<int>(coins.size(),-1));
         int t=ways(0,amount,coins,dp);
         return t;
