8aaa4ca6ae3097c13643354c30126ff26811eb4e
Rajat Raj <rajatyadav2k17@gmail.com>
2022-05-10 23:31:16 +0530

    Time: 7 ms (12.52%), Space: 13.5 MB (5.40%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,33 @@
+void ways(int idx,int sum,vector<int>v,vector<vector<int>>&res,int k,int n)
+{
+    if(v.size()==k || sum>n)
+    {
+        if(sum==n)
+        {
+            res.push_back(v);
+        }
+        return;
+    }
+    
+    if(idx<10)
+        ways(idx+1,sum,v,res,k,n);
+     
+    if(idx<10)
+    {
+        v.push_back(idx);
+        ways(idx+1,sum+idx,v,res,k,n);
+        v.pop_back();
+    }
+}
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int>v;
+        vector<vector<int>>res;
+        ways(1,0,v,res,k,n);
+        
+        return res;
+    }
+};
+
+// 1,2,3,4,5,6,7,8,9
